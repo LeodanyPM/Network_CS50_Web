901ed647befdcc378d2cdf19ee2392aa8ec1401c
lpm <leodanypm@gmail.com>
2026-07-18 22:10:32 -0400

    Modelos Post y Like

--- network/models.py
+++ network/models.py
@@ -4,3 +4,25 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="post" )
+    date = models.DateTimeField(auto_now_add=True)
+    body = models.TextField()
+    
+    @property
+    def likes_count(self):
+        return self.likes.count()
+        
+    class Meta:
+         ordering = ['-date']
+    def __str__(self):
+         return f"{self.user} post at {self.date}"
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name="user_likes")
+        
+    class Meta:
+        unique_together = ['post', 'user']
+    def __str__(self):
+        return f"{self.user} liked {self.post}"
